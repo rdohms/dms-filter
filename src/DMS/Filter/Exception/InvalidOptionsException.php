7bf7dfb8640eb04f9d39de95d3f2716f1c3526fa
Rafael Dohms <rdohms@gmail.com>
2011-07-20 23:15:09 -0300

    [DMS] Adding documentation, build.xml phing file and updating ignores [Filter] Fixing documentation for Exceptions and adding Version class for version tracking

--- src/DMS/Filter/Exception/InvalidOptionsException.php
+++ src/DMS/Filter/Exception/InvalidOptionsException.php
@@ -2,6 +2,13 @@
 
 namespace DMS\Filter\Exception;
 
+/**
+ * Invalid option passed to rule
+ * 
+ * @package DMS
+ * @subpackage Filter
+ * @category Exception
+ */
 class InvalidOptionsException extends RuleOptionsException
 {
       
