6b77d45b2d97507d7e5ed2fc5ab8ff3ba1f81428
Rafael Dohms <rdohms@gmail.com>
2012-04-25 19:59:29 +0200

    Html entities Filter
    Implemented filter to leverage html entities, closes #9.

---
+++ Rules/HtmlEntities.php
@@ -0,0 +1,43 @@
+<?php
+namespace DMS\Filter\Rules;
+
+/**
+ * Html Entities Filter
+ *
+ * @package DMS
+ * @subpackage Filter
+ *
+ * @Annotation
+ */
+class HtmlEntities extends Rule
+{
+    /**
+     * Flags
+     *
+     * @var int
+     */
+    public $flags = ENT_COMPAT;
+
+    /**
+     * Encoding to be used
+     *
+     * @var string
+     */
+    public $encoding = 'UTF-8';
+
+    /**
+     * Convert existing entities
+     *
+     * @var bool
+     */
+    public $doubleEncode = true;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function applyFilter($value)
+    {
+        return htmlentities($value, $this->flags, $this->encoding, $this->doubleEncode);
+    }
+
+}
