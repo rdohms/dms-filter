36536f13c4edf261077ad85cc974b61c8bfbbd16
Sergey Obraztsov <sergey@DESKTOP-UU0TD4V.localdomain>
2023-09-05 14:00:46 +0300

    added test for attribute loader class

---
+++ tests/DMS/Filter/Mapping/Loader/AttributeLoaderTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DMS\Filter\Mapping\Loader;
+
+use DMS\Filter\Mapping\ClassMetadata;
+use DMS\Tests\Dummy\Classes\AttributedClass;
+use DMS\Tests\FilterTestCase;
+use ReflectionProperty;
+
+final class AttributeLoaderTest extends FilterTestCase
+{
+    public function testLoadMetadata(): void
+    {
+        $loader   = new AttributeLoader();
+        $metadata = new ClassMetadata(AttributedClass::class);
+        
+        $loadMetadataResult = $loader->loadClassMetadata($metadata);
+        
+        $this->assertTrue($loadMetadataResult);
+        
+        $classProperties = array_map(
+            fn (ReflectionProperty $property) => $property->getName(), 
+            $metadata->getReflectionClass()->getProperties()
+        );
+        
+        $this->assertSame($classProperties, $metadata->getFilteredProperties());
+    }
+}
